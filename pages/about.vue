96872f368617f979609e98b3d7ce7e8e4b229cde
lucas-cq <lucas.cummings024@gmail.com>
2021-11-26 11:04:40 -0700

    about page update

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <!--Nav Here-->
-    
+      <section>
+        <h1>About Us</h1>
+        <p>We are a passion driven team looking to make a difference
+          in the web development industry.
+        </p>
+        <div>
+          <nuxt-img src="assets/images/clipboard.png" />
+        </div>
+    </section>
     <!--Footer Here-->
   </div>
 </template>
