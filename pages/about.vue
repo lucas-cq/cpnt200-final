0378a3149d10764670f6da5c481d2492811e2273
lucas-cq <lucas.cummings024@gmail.com>
2021-11-25 18:35:30 -0700

    tweaks

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!--Nav Here-->
+    
+    <!--Footer Here-->
+  </div>
+</template>
+
+<style scoped lang="postcss">
+
+</style>
+
+<script>
+export default {
+  
+}
+</script>
+
