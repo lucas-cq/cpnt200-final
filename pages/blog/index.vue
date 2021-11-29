d3c169b5a5283dbfc03bbe56c59d4157be0ecbb4
lucas-cq <lucas.cummings024@gmail.com>
2021-11-29 16:53:15 -0700

    Article landing page done

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <v-main>
-    <h1>Articles</h1>
-    <div v-for="post in posts" :key="post.slug">
-      <nuxt-link :to="`blog/${post.slug}`">{{ post.title }}</nuxt-link>
-    </div>
-  </v-main>
+  <div class="font-Poppins">
+    <Navigation />
+    <section class="flex justify-around flex-wrap mx-auto bg-indigo-light">
+      <article class="mx-20 mb-40 mt-52">
+        <h1 class="text-5xl mb-10 text-purple-med"><strong>Articles</strong></h1>
+        <p class="text-2xl tracking-wide leading-relaxed">A collection of our finest articles written by nerds <br>like you.</p>
+      </article>
+        <div class="img-box">
+          <nuxt-img src="assets/images/pencil-case.png" />
+        </div>
+    </section>
+    <div class="article-box">
+        <div class="article-li" v-for="post in posts" :key="post.slug">
+          <nuxt-link class="text-4xl hover:text-purple-med" :to="`blog/${post.slug}`">{{ post.title }}</nuxt-link>
+          <p class="text-2xl pt-6">{{ post.description }}</p>
+        </div>
+      </div>
+    <Footer />
+  </div>
 </template>
 
 <style scoped lang="postcss">
+.article-box {
+  @apply bg-indigo-dark flex justify-center flex-col flex-wrap mx-auto
+}
+
+.article-li {
+  @apply my-20 pl-64
+}
+
+.img-box {
+  @apply w-96 mt-32
+}
 
 </style>
 
