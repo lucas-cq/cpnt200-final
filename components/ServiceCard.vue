24fa86255a649128e6142dac7f102124c24f7c88
Raj-Hunjan <rajhunjann@gmail.com>
2021-11-28 21:39:45 -0700

    service page still under construction 🛠

---
+++ components/ServiceCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="card-box">
+    <div class="member-card">
+      <h3 class="text-2xl text-center mt-4">{{ info.title }}</h3>
+      <p class="text-center text-lg">{{ info.para }}</p>
+    </div >
+  </section>
+</template>
+
+<style scoped lang="postcss">
+  .card-box {
+    @apply my-20 pb-16 pt-10 px-12 bg-indigo-light rounded-md
+  }
+</style>
+
+<script>
+export default {
+  props: ['info']
+}
+</script>
---
+++ pages/services.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+    <!--Nav Here-->
+    <section class="flex justify-around flex-wrap mx-auto bg-indigo-light">
+      <article class="m-20">
+      <h1 class="text-5xl mb-10 text-purple-med"><strong>Services</strong></h1>
+        <p class="text-2xl tracking-wide leading-relaxed">Something about our services
+        </p>
+      </article>
+        <div class="img-box">
+          <nuxt-img src="assets/images/linechart.png" />
+        </div>
+    </section>
+    <section class="bg-indigo-dark">
+      <h2 class="text-center pt-20 text-4xl text-purple-med"><strong>What We Offer</strong></h2>
+      <div class="service-box">
+        <servicecard v-for="service in services" :key="service.id" :info="service" />
+      </div>
+    </section>
+    <!--Footer Here-->
+  </div>
+</template>
+
+<!-- <style lang="postcss">
+.img-box {
+  @apply w-96
+}
+.service-box {
+  @apply flex justify-around flex-wrap mx-60
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      information: [
+        {
+         
+        },
+        {
+         
+        },
+        {
+          
+        },
+        {
+          
+        }
+      ]
+    }
+  }
+}
+</script> -->
