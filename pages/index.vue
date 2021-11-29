e1ecca66f368f9d70ac5c05f9a25ce4b3756bf5c
David Ha <David.ha0908@gmail.com>
2021-11-28 21:02:28 -0700

    created index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="font-Poppins">
+  <div class="font-Poppins relative">
     <Navigation/>
-    <main>
-      <div>
-        <h1>The Creative Agency for <span>Innovative</span> Digital Experiences</h1>
-        <nuxt-link to="/contact">Let's Talk</nuxt-link>
+    <main class="h-screen bg-indigo-light">
+      <div class="container h-full flex justify-center items-center">
+        <div class="w-1/2">
+          <h1 class="font-bold text-6xl leading-normal mb-8">The Creative Agency<br>For <span class="text-purple-med">Innovative</span><br>Digital Experiences</h1>
+          <nuxt-link to="/contact" class="bg-purple-light px-4 py-1 rounded text-xl">Let's Talk</nuxt-link>
+        </div>
+        <div>
+          <nuxt-img src="/assets/images/computer.png" sizes="sm:100vw md:50vw lg:40vw"/>
+        </div>
       </div>
     </main>
 
