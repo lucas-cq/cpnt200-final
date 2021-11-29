931fabe7a7fe4a9870a6d56add4e2af80240d884
David Ha <David.ha0908@gmail.com>
2021-11-28 21:23:36 -0700

    created contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -14,9 +14,30 @@
       </div>
     </main>
 
-    <section class="h-screen bg-indigo-dark">
+    <section class="h-screen bg-indigo-dark flex flex-col items-center justify-center">
       <div>
-        <h1 class="font-bold text-6xl leading-normal text-purple-med border-b-2 border-black w-1/4">Contact Us</h1>
+        <h1 class="font-bold text-6xl leading-normal text-purple-med border-b-2 border-black w-max">Contact Us</h1>
+      </div>
+
+      <div class="flex gap-8 my-20">
+        <div class="border-0 bg-indigo-light p-12 text-center flex flex-col gap-4 rounded-md">
+          <h2 class="text-2xl font-bold">Location</h2>
+          <p>100 10th Ave SW</p>
+          <p>Calgary, Alberta</p>
+        </div>
+        <div class="border-0 bg-indigo-light p-12 text-center flex flex-col gap-4 rounded-md">
+          <h2 class="text-2xl font-bold">Phone</h2>
+          <p>(403) 888-8888</p>
+        </div>
+        <div class="border-0 bg-indigo-light p-12 text-center flex flex-col gap-4 rounded-md">
+          <h2 class="text-2xl font-bold">Email</h2>
+          <p>info@vexus.com</p>
+        </div>
+        <div class="border-0 bg-indigo-light p-12 text-center flex flex-col gap-4 rounded-md">
+          <h2 class="text-2xl font-bold">Hours</h2>
+          <p>Monday-Friday</p>
+          <p>8am - 5pm</p>
+        </div>
       </div>
     </section>
 
