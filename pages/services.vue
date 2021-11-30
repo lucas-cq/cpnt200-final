29dd674689430aa5312d8881cbeb49de491a943c
Raj-Hunjan <rajhunjann@gmail.com>
2021-11-29 19:37:03 -0700

    services page almost done, just cards are left

--- pages/services.vue
+++ pages/services.vue
@@ -1,22 +1,21 @@
 <template>
-<div>
+  <div>
     <Navigation/>
-    <section class="flex justify-around flex-wrap mx-auto bg-indigo-light">
-      <article class="mx-20 mb-40 mt-52">
-      <h1 class="text-5xl mb-10 text-purple-med"><strong>Services</strong></h1>
-        <p class="text-2xl tracking-wide leading-relaxed"> Our team specializes in a variety <br> of services to best suit <br>our clients needs
-        </p>
-      </article>
+      <section class="flex justify-around flex-wrap mx-auto bg-indigo-light">
+        <article class="mx-20 mb-40 mt-52">
+          <h1 class="text-5xl mb-10 text-purple-med"><strong>Services</strong></h1>
+          <p class="text-2xl tracking-wide leading-relaxed"> Our team specializes in a variety <br> of services to best suit <br> our customers needs</p>
+        </article>
         <div class="img-box">
           <img src="assets/images/linechart.png" />
         </div>
-    </section>
-    <section class="bg-indigo-dark">
-      <h2 class="text-center pt-20 text-4xl text-purple-med"><strong>What We Offer</strong></h2>
-      <div class="service-box">
-        <ServiceCard v-for="service in services" :key="service.id" :info="service" />
-      </div>
-    </section>
+      </section>
+      <section class="bg-indigo-dark">
+        <h2 class="text-center pt-20 text-4xl text-purple-med"><strong>What We Offer</strong></h2>
+        <div class="service-box">
+          <ServiceCard v-for="service in services" :key="service.id" :info="service" />
+        </div>
+      </section>
     <Footer/>
   </div>
 </template>
@@ -37,19 +36,19 @@ export default {
       services: [
         {
          title: "UX/UI design",
-         para: "We have excellent UX/UI "
+         para: "We have excellent UX/UI designs so that you have a website you're proud to show off"
         },
         {
          title: "Backend Development",
-         para: ""
+         para: "Amazing backend development to ensure you're website has the best performance"
         },
         {
          title: "Graphic Design",
-         para: ""
+         para: "We have great graphic design for your wonderful website"
         },
         {
          title: "SEO",
-         para: ""
+         para: "We'll make sure that your website is ranked up top with other websites and user friendly"
         }
       ]
     }
