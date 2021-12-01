f79bbb2ba2b2130fe32e4ff4086fe5856c7a881d
David Ha <David.ha0908@gmail.com>
2021-12-01 14:17:30 -0700

    styled page to be more consistent

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="font-Poppins">
     <Navigation />
-    <section class="flex justify-around flex-wrap mx-auto bg-indigo-light">
-      <article class="mx-20 mb-40 mt-52">
-      <h1 class="text-5xl mb-10 text-purple-med"><strong>About Us</strong></h1>
-        <p class="text-2xl tracking-wide leading-relaxed">We are a passion driven team looking to <br>make a difference
-          in the web <br> development industry.
-        </p>
-      </article>
-        <div class="img-box">
-          <nuxt-img src="assets/images/clipboard.png" />
-        </div>
+    <section class="bg-indigo-light h-screen">
+      <div class="container flex justify-between items-center flex-wrap h-full">
+        <article class="w-1/2">
+        <h1 class="text-6xl leading-normal text-purple-med"><strong>About Us</strong></h1>
+          <p class="text-3xl leading-normal">We are a passion driven team looking to make a difference
+            in the web development industry.
+          </p>
+        </article>
+          <div>
+            <nuxt-img src="assets/images/clipboard.png" sizes="sm:100vw md:50vw lg:30vw" />
+          </div>
+      </div>
     </section>
-    <section class="bg-indigo-dark">
-      <h2 class="text-center pt-20 text-4xl text-purple-med"><strong>Our Team</strong></h2>
-      <div class="team-box">
-        <Teamcard v-for="team in teams" :key="team.id" :info="team" />
+    <section class="bg-indigo-dark h-screen">
+      <div class="flex flex-col items-center justify-center gap-8">
+        <h2 class="text-center pt-20 text-6xl text-purple-med border-b-2 border-black w-max"><strong>Our Team</strong></h2>
+        <div class="team-box">
+          <Teamcard v-for="team in teams" :key="team.id" :info="team" />
+        </div>
       </div>
     </section>
     <Footer />
@@ -23,11 +27,8 @@
 </template>
 
 <style lang="postcss">
-.img-box {
-  @apply w-96 mt-32
-}
 .team-box {
-  @apply flex justify-around flex-wrap mx-60
+  @apply flex justify-around flex-wrap mx-60 gap-8
 }
 </style>
 
