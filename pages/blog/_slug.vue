43a8c546baf87633fd996ba77849c5f7640e05bd
lucas-cq <lucas.cummings024@gmail.com>
2021-12-05 16:32:58 -0700

    added yt iframe

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -6,8 +6,9 @@
       <h1 class="text-4xl underline mb-4">{{ post.title }}</h1>
       <h2 class="text-2xl mb-6">{{ post.description }}</h2>
       <nuxt-img :src="post.banner" height="600" />
-      <nuxt-content class="text-xl leading-10 w-1/2" :document="post" />
+      <nuxt-content class="text-xl leading-10 w-1/2 mt-8" :document="post" />
     </article>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/OrxmtDw4pVI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </section>
     <Footer />
   </div>
